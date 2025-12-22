27476280ff68f2b8a15312c8e1a8eb4de4d7d2dd
hojin <eunhj89@gmail.com>
2025-12-22 19:12:23 +0900

    first commit

---
+++ src/main/java/com/unocode/foremost/ForemostApplication.java
@@ -0,0 +1,13 @@
+package com.unocode.foremost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForemostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForemostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unocode/foremost/ForemostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unocode.foremost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForemostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
